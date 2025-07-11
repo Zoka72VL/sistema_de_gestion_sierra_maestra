50c927afe22e0f165da12ce01171d74ba8615629
Huguito <110265653+BondiolaAlHorno@users.noreply.github.com>
2025-07-11 19:27:21 -0300

    primer commit de qa

---
+++ src/main/java/com/sierramaestra/service/BarrileServicio.java
@@ -0,0 +1,30 @@
+package com.sierramaestra.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.sierramaestra.model.Barril;
+
+public interface BarrileServicio {
+List<Barril> listarTodosLosBarriles();
+    
+    Barril guardarBarril(Barril barril);
+    
+    Barril obtenerBarrilPorId(Long id);
+    
+    Barril actualizarBarril(Barril barril);
+    
+    void eliminarBarril(Long id);
+
+    List<Barril> listarBarrilesPorEstado(String estado);
+    
+    Page<Barril> listarTodosLosBarriles(Pageable pageable);
+    
+    Page<Barril> listarBarrilesPorEstado(String estado, Pageable pageable);
+    
+    List<Barril> listarBarrilesPorEstadoLimpio();
+    
+    List<Barril> listarBarrilesPorLote(Long loteId);
+}
